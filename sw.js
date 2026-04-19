b8da93b1668088f0cdb0fe5dd3c837b107508a24
prawnsolo <solutions@proactivepixels.com>
2026-04-19 05:32:07 -0400

    Add PWA support for iPhone web app (icons, manifest, service worker)

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'holocron-v1';
+const ASSETS = [
+  'holocron.html',
+  'css/tokens.css',
+  'css/base.css',
+  'css/components.css',
+  'js/state.js',
+  'js/router.js',
+  'js/dashboard.js',
+  'js/projects.js',
+  'js/memory.js',
+  'js/weekly.js',
+  'assets/logo.svg',
+  'assets/icon-192.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
